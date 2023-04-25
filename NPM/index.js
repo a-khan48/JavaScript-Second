9734005dba83c8653ebbcb6a1646b9586978e9f8
Ahmad Khan <ahmad.d.khan40@gmail.com>
2023-04-25 13:16:28 -0400

    moment.js

---
+++ NPM/index.js
@@ -0,0 +1,9 @@
+// import moment from "moment/moment";
+
+const moment = require('moment')
+
+const myMoment = new moment()
+
+console.log(myMoment.format('h:mm:ss a'))
+
+console.log(myMoment.format());
